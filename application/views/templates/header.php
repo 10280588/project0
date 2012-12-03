8e9690bb924242125850a55977172e179216a132
10280588 <mennovleeuwen1990@gmail.com>
2012-12-03 06:34:17 -0500

    prototyping views

---
+++ application/views/templates/header.php
@@ -0,0 +1,21 @@
+<?php $this->load->helper('html'); ?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes" /> 
+    
+    <title>the begin</title>
+    <?php echo link_tag('css/themes/harvardColor.min.css'); ?>
+    <?php echo link_tag('css/main.css'); ?> 
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile.structure-1.2.0.min.css" />
+    <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+</head>
+
+<body>
+
+   
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -10,8 +10,16 @@
 	::moz-selection{ background-color: #E13300; color: white; }
 	::webkit-selection{ background-color: #E13300; color: white; }
 
-	body {
-		background-color: #fff;
+standard {
+    position:;
+    margin:;
+    padding:;
+    font-size:;
+    color:;
+    background-color:;
+}
+	body {	
+		background-color: #fff;		
 		margin: 40px;
 		font: 13px/20px normal Helvetica, Arial, sans-serif;
 		color: #4F5155;
@@ -70,7 +78,7 @@
 	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>The page you arett looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
