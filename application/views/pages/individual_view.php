01f35648951d8622c27af1498924ece2f83a94ac
10280588 <mennovleeuwen1990@gmail.com>
2012-12-05 05:30:13 -0500

    Dasyels commit from Menno's laptop

--- application/views/pages/individual_view.php
+++ application/views/pages/individual_view.php
@@ -1,96 +1,131 @@
+<?php 
+reset($courses);
+$course = current($courses);
+reset($schedule);
+$sched = current($schedule);
+?>
+
+<!-- begin of page -->
 <div data-role="page" class="individual" data-theme="a" >
-
+    
+    <!-- begin of header -->
 	<div data-role="header" data-position="inline">
-	<a href="index.html" data-role="button" data-rel="back" data-icon="arrow-l" data-iconpos="notext">Back</a>
-	 <?php foreach($courses as $course): ?>
-	<h1>Course Info</h1>
-	<a href="index.html" data-icon="plus" class="ui-btn-right">Add</a>
+	    <a href="index.html" data-role="button" data-rel="back" data-icon="arrow-l" data-iconpos="notext">Back</a>
+	    <h1>Course Info</h1>
+	    <a href="index.html" data-icon="plus" class="ui-btn-right">Add</a>
 	</div>
-
-<?php /**  Your app must enable users to see courses’ catalog numbers,
- titles, departments, descriptions, instructors, locations, and days and times. */ ?>
- 
-<div data-role="content">
-   
-    <h2><?php echo $course['title'];?></h2>
-    <div data-role="collapsible" data-inset="false">
-        <h3>Description</h3>
-        <p id="description" ><?php echo $course['description'];?></p>
-    </div>
-    <div data-role="collapsible" data-inset="false">
-        <h3>Additional Information</h3>
-        <p><span class="bold">Department:</span> Earth and Planetary Sciences</p>
-        <p><span class="bold">Catalog number:</span> <?php echo $course['cat_num'];?></p>
-        <p><span class="bold">When:</span> <?php echo $course['year'];?></p>
-        <p><span class="bold">Instructors:</span> <?php echo $course['year'];?></p>
-        <p><span class="bold">Locations:</span>
-    </div>
-    <div data-role="collapsible" data-inset="false">
-        <h3>Notes</h3>
-        <p id="description" ><?php echo $course['notes'];?></p>
-    </div>
+    <!-- end of header -->
+    
+    <!-- begin of content -->
+    <div data-role="content">
+        <h2><?php echo $course['title'];?></h2>
+        
+        <!-- begin of description -->
+        <div data-role="collapsible" data-inset="false">
+            <h3>Description</h3>
+            <p id="description" ><?php echo $course['description'];?></p>
+        </div>
+        <!-- end of description -->
+        
+        <!--begin of additional information -->
+        <div data-role="collapsible" class="addInfo" data-inset="false">
+            <h3>Additional Information</h3>
+            <p>
+                <span class="bold red block">Department: </span> <?php echo ( empty($course['short_name']) ? 'No info' :  $course['short_name'] ); ?>
+            </p>
+            <p>
+                <span class="bold red block">Catalog number:</span> <?php echo ( empty($course['cat_num']) ? 'No info' :  $course['cat_num'] ); ?>
+            </p>
+            <p>
+                <span class="bold red block">When:</span> <?php echo  $sched['name'] .' '. $course['year'] ;?>
+            </p>
+            <p></p>
+                <span class="bold red block">Instructors:</span>
+                <table>
+                    <tr>
+                        <td  class="title">Name</td>
+                        <td  class="title center quarter">Role</td>
+                    </tr>
+                    <?php foreach($faculty as $facl): ?>
+                    <tr>
+                        <td><?php echo $facl['prefix'] .' '. $facl['first_name'] .' '. $facl['middle_name'] .' '. $facl['last_name'] .' '. $facl['suffix']; ?></td>
+                        <td class="center quarter"><?php echo $facl['role'];?></td>
+                    </tr>
+                    <?php endforeach ?>
+                </table>
+               
+            
+            <p></p>
+                <span class="bold red block">Locations:</span>
+                <table>
+                    <tr>
+                        <td  class="title">Building</td>
+                        <td  class="title center quarter ">Room</td>
+                    </tr>
+                    <?php foreach($locations as $location): ?>
+                    <tr>
+                        <td><?php echo $location['building']; ?></td>
+                        <td class="center quarter"><?php echo $location['room']; ?></td>
+                    </tr>
+                    <?php endforeach ?>
+                </table>
+           
+        </div>
+        <!-- end of additional information -->
+        
+        <!-- begin of notes -->
+        <div data-role="collapsible" data-inset="false">
+            <h3>Notes</h3>
+            <p><?php echo $course['notes'];?></p>
+        </div>
+        <!-- end of notes -->
     
     
-    <input type="submit" value="Add to Basket" >
+        <input type="submit" value="Add to Basket" >
+    </div>
+    <!-- end of content -->
     
-</div>
-
-    <div id="greyArea">
+    <!-- begin of red schedule -->
+    <div id="redArea" >
         <div id="content" data-role="content">
-            <table>
-                <tr>
-                <th>Type</th>
-                <th>Day</th>
-                <th class="time">Time</th>
-                <th>Req</th>
-                <tr>
-                    <td>Lecture</td>
-                    <td>Mon</td>
-                    <td class="time">
-                       29:00-22:00
-                    </td>
-                    <td> Yes </td>
-                </tr>
-                <tr>
-                    <td>Lecture</td>
-                    <td>Mon</td>
-                    <td class="time">
-                       29:00-22:00
-                    </td>
-                    <td> Yes </td>
-                </tr>
-                <tr>
-                    <td>Lecture</td>
-                    <td>Mon</td>
-                    <td class="time">
-                       29:00-22:00
-                    </td>
-                    <td> Yes </td>
-                </tr>
-                <tr>
-                    <td>Lecture</td>
-                    <td>Mon</td>
-                    <td class="time">
-                       9:00-1:00
-                    </td>
-                    <td> Yes </td>
-                </tr>
-            </table>
+            <?php if (empty($schedule)): ?>
+                <p class="white">No schedule information available</p>
+            <?php else: ?>
+                <table>
+                    <tr>
+                        <th>Type</th>
+                        <th>Day</th>
+                        <th class="center">Time</th>
+                        <th class="center">Optional</th>
+                    </tr>
+                    <?php foreach($schedule as $schedul): ?>
+                        <tr>
+                            <td>Lecture</td>
+                            <td><?php echo $schedul['short_name']?></td>
+                            <td class="center">
+                               <?php echo $schedul['begin_time']?>-<?php echo $schedul['end_time']?>
+                            </td>
+                            <td class="center"><?php echo $schedul['optional']?></td>
+                        </tr>
+                    <?php endforeach ?>
+                </table>
+            <?php endif ?>
         </div>
     </div>
-   
+    <!-- end of red schedule -->
     
-    <?php endforeach ?>
+    <!-- begin of footer -->
+    <div data-role="footer" data-position="fixed">
+	    <div data-role="navbar">
+		    <ul>
+			    <li><a href="/"  data-icon="home">Home</a></li>
+			    <li><a href="#"  data-icon="search">Search</a></li>
+			    <li><a href="#"  data-icon="alert">Basket</a></li>
+			    <li><a href="#"  data-icon="info">Info</a></li>
+		    </ul>
+	    </div>
+    </div>
+    <!-- end of footer -->
 
-
-
-<div data-role="footer" data-position="fixed">
-		<div data-role="navbar">
-		<ul>
-			<li><a href="/"  data-icon="home">Home</a></li>
-			<li><a href="#"  data-icon="search">Search</a></li>
-			<li><a href="#"  data-icon="alert">Basket</a></li>
-			<li><a href="#"  data-icon="info">Info</a></li>
-		</ul>
-	</div><!-- /navbar -->
-	</div><!-- /footer -->
+</div>
+<!-- end of page -->
