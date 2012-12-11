d410584708328b4ec2d5c7c42d25395398883d73
10280588 <mennovleeuwen1990@gmail.com>
2012-12-11 09:49:33 -0500

    login view added

---
+++ application/controllers/home.php
@@ -0,0 +1,18 @@
+<?php
+class Home extends CI_Controller {
+
+
+
+	public function index()
+	{
+	    $this->load->helper('url');
+	    $this->load->helper('login');
+	    check_logged_in();
+		$this->load->view('templates/header');
+		$this->load->view('pages/home_view');
+		$this->load->view('templates/footer');
+	}
+
+}
+
+
